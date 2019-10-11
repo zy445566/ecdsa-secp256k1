6bd88c9bf988a5b50119a457be82ceb5357b8990
zy445566 <zyct@vip.qq.com>
2019-10-11 12:32:39 +0800

    支持字符串类型数字传入

--- index.js
+++ index.js
@@ -3,27 +3,67 @@ const ecc = require('./lib')
 
 let pointG = ecc.num2Point(secp256k1Config.G);
 
+function StringConvertBigNum(num) {
+    if(typeof num == 'string') {
+        return BigInt(num);
+    }
+    return num;
+}
 
 function randPrivateKeyNum() {
     return ecc.getPrivteKeyNumByRand(secp256k1Config.n);
 }
 
+function publicKeyNum2Point(publicKeyNum) {
+    return ecc.num2Point(StringConvertBigNum(publicKeyNum));
+}
+
+function publicKeyPoint2HexStr(publicKeyPoint) {
+    return ecc.point2HexStr({
+        x:StringConvertBigNum(publicKeyPoint.x),
+        y:StringConvertBigNum(publicKeyPoint.y)
+    });
+}
+
+function publicKeyPoint2Num(publicKeyPoint) {
+    return ecc.point2Num({
+        x:StringConvertBigNum(publicKeyPoint.x),
+        y:StringConvertBigNum(publicKeyPoint.y)
+    });
+}
+
+
 function getPublicKeyPoint(privateKeyNum) {
-    return ecc.getPointByNum(privateKeyNum,pointG,secp256k1Config.p,secp256k1Config.a);
+    return ecc.getPointByNum(
+        StringConvertBigNum(privateKeyNum),
+        pointG,secp256k1Config.p,secp256k1Config.a
+    );
 }
 
 function sign(privateKeyNum,msgDataNum) {
-    return ecc.sign(secp256k1Config.n,pointG,secp256k1Config.p,secp256k1Config.a,privateKeyNum,msgDataNum);
+    return ecc.sign(
+        secp256k1Config.n,pointG,secp256k1Config.p,secp256k1Config.a,
+        StringConvertBigNum(privateKeyNum),
+        StringConvertBigNum(msgDataNum)
+    );
 }
 
 function verify(publicKeyPoint,signData,msgDataNum) {
-    return ecc.verify(secp256k1Config.n,pointG,secp256k1Config.p,secp256k1Config.a,publicKeyPoint,signData,msgDataNum);
+    return ecc.verify(
+        secp256k1Config.n,pointG,secp256k1Config.p,secp256k1Config.a,
+        {
+            x:StringConvertBigNum(publicKeyPoint.x),
+            y:StringConvertBigNum(publicKeyPoint.y)
+        },
+        StringConvertBigNum(signData),
+        StringConvertBigNum(msgDataNum)
+    );
 }
 
 module.exports = {
-    publicKeyNum2Point:ecc.num2Point,
-    publicKeyPoint2HexStr:ecc.point2HexStr,
-    publicKeyPoint2Num:ecc.point2Num,
+    publicKeyNum2Point,
+    publicKeyPoint2HexStr,
+    publicKeyPoint2Num,
     randPrivateKeyNum,
     getPublicKeyPoint,
     sign,
--- test.js
+++ test.js
@@ -91,7 +91,88 @@ let testUnit = {
             && ecdsa.verify(publicKeyErrorPoint,signData,msgDataNum)===false
             && ecdsa.verify(publicKeyPoint,signErrorData,msgDataNum)===false
             && ecdsa.verify(publicKeyPoint,signData,msgDataErrorNum)===false
-        ,'sign error!');
+        ,'verify error!');
+    },
+
+    [Symbol('test.publicKeyNum2Point(String)')] : async function() {
+        let publicKeyNum = '0x04ea279824636aa9172473b6c3076727f17dada14847305487405a38a09c91ce6d63478f426ddcf618be66568cb6bd5bd7201c71689705d9602ae0a7c131a3bafb';
+        let publicKeyPoint = ecdsa.publicKeyNum2Point(publicKeyNum);
+        assert.deepStrictEqual(publicKeyPoint,{
+            x: 0xea279824636aa9172473b6c3076727f17dada14847305487405a38a09c91ce6dn,
+            y: 0x63478f426ddcf618be66568cb6bd5bd7201c71689705d9602ae0a7c131a3bafbn
+        },'publicKeyNum2Point(String) error!')
+    },
+    [Symbol('test.publicKeyPoint2HexStr(String)')] : async function() {
+        let publicKeyPoint = {
+            x: '0xea279824636aa9172473b6c3076727f17dada14847305487405a38a09c91ce6d',
+            y: '0x63478f426ddcf618be66568cb6bd5bd7201c71689705d9602ae0a7c131a3bafb'
+        };
+        let publicKeyNumHexStr = ecdsa.publicKeyPoint2HexStr(publicKeyPoint);
+        assert.strictEqual(publicKeyNumHexStr,
+            `0x04ea279824636aa9172473b6c3076727f17dada14847305487405a38a09c91ce6d63478f426ddcf618be66568cb6bd5bd7201c71689705d9602ae0a7c131a3bafb`,
+            'publicKeyPoint2HexStr(String) error!'
+        );
+    },
+    [Symbol('test.publicKeyPoint2Num(String)')] : async function() {
+        let publicKeyPoint = {
+            x: '0xea279824636aa9172473b6c3076727f17dada14847305487405a38a09c91ce6d',
+            y: '0x63478f426ddcf618be66568cb6bd5bd7201c71689705d9602ae0a7c131a3bafb'
+        };
+        let publicKeyNum = ecdsa.publicKeyPoint2Num(publicKeyPoint);
+        assert.strictEqual(
+            publicKeyNum,
+            0x04ea279824636aa9172473b6c3076727f17dada14847305487405a38a09c91ce6d63478f426ddcf618be66568cb6bd5bd7201c71689705d9602ae0a7c131a3bafbn,
+            'publicKeyPoint2Num error!'
+        );
+    },
+    [Symbol('test.getPublicKeyPoint(String)')] : async function() {
+        let privateKeyNum = '0xeddbdc1168f1daeadbd3e44c1e3f8f5a284c2029f78ad26af98583a499de5b19';
+        let publicKeyPoint = ecdsa.getPublicKeyPoint(privateKeyNum);
+        assert.deepStrictEqual(publicKeyPoint,{
+            x: 0xea279824636aa9172473b6c3076727f17dada14847305487405a38a09c91ce6dn,
+            y: 0x63478f426ddcf618be66568cb6bd5bd7201c71689705d9602ae0a7c131a3bafbn
+        },'getPublicKeyPoint(String) error!')
+    },
+    [Symbol('test.sign(String)')] : async function() {
+        let privateKeyNum = '0xeddbdc1168f1daeadbd3e44c1e3f8f5a284c2029f78ad26af98583a499de5b19';
+        let publicKeyPoint = {
+            x: 0xea279824636aa9172473b6c3076727f17dada14847305487405a38a09c91ce6dn,
+            y: 0x63478f426ddcf618be66568cb6bd5bd7201c71689705d9602ae0a7c131a3bafbn
+        }
+        let msgData = 'Test Data';
+        let sha1 = crypto.createHash('sha256').update(msgData).digest('hex');
+        let msgDataNum = `0x${sha1}`;
+        let signData = ecdsa.sign(privateKeyNum,msgDataNum);
+        assert(ecdsa.verify(publicKeyPoint,signData,msgDataNum),'sign(String) error!');
+    },
+    [Symbol('test.verify(String)')] : async function() {
+        let privateKeyNum = '0xeddbdc1168f1daeadbd3e44c1e3f8f5a284c2029f78ad26af98583a499de5b19';
+        let publicKeyPoint = {
+            x: '0xea279824636aa9172473b6c3076727f17dada14847305487405a38a09c91ce6d',
+            y: '0x63478f426ddcf618be66568cb6bd5bd7201c71689705d9602ae0a7c131a3bafb'
+        }
+        let msgData = 'Test Data';
+        let sha1 = crypto.createHash('sha256').update(msgData).digest('hex');
+        let msgDataNum = `0x${sha1}`;
+        let signData = ecdsa.sign(privateKeyNum,msgDataNum);
+        //  ---Error Date---
+        let randNum = function(){return BigInt(Math.round(Math.random()*100000));}
+        let publicKeyErrorPoint = {
+            x: (publicKeyPoint.x+randNum()).toString(16),
+            y: (publicKeyPoint.y+randNum()).toString(16),
+        }
+        let signErrorData = {
+            r:(signData.r+randNum()).toString(16),
+            s:(signData.s+randNum()).toString(16),
+        }
+        let msgDataErrorNum = msgDataNum+randNum();
+        //  ---Error Date---
+        assert(
+            ecdsa.verify(publicKeyPoint,signData,msgDataNum) === true 
+            && ecdsa.verify(publicKeyErrorPoint,signData,msgDataNum)===false
+            && ecdsa.verify(publicKeyPoint,signErrorData,msgDataNum)===false
+            && ecdsa.verify(publicKeyPoint,signData,msgDataErrorNum)===false
+        ,'verify(String) error!');
     },
 }
 
